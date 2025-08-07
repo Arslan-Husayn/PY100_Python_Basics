e517b7418233c536343dceeeebab9ebe3fa663e9
Talha Husayn <220093113+Arslan-Husayn@users.noreply.github.com>
2025-08-06 20:05:40 -0700

    re-pushing

---
+++ Reading_Documentation_2/string_formatting.py
@@ -0,0 +1,9 @@
+name = "Victor"
+profession = "programmer"
+
+# using str.format
+print("Hello, {}. You are a {}.".format(name, profession))
+
+# using f-string format
+
+print(f"Hello, {name}. You are a {profession}.")

1b79a2e1f68d5fba3e5aa84395ea4ef4b2b4bb97
Talha Husayn <220093113+Arslan-Husayn@users.noreply.github.com>
2025-08-09 07:46:53 -0700

    needed to save code

---
+++ Reading_Documentation_2/DateTime.py
@@ -0,0 +1,13 @@
+#import module and class
+
+from datetime import datetime   
+
+from datetime import date
+
+#set a variable to today
+
+today_day_time = datetime.today()
+
+print(today_day_time)
+
+

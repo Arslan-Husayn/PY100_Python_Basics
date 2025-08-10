b2d8519b4f19075b27aff55c822eb71133aaeca4
Talha Husayn <220093113+Arslan-Husayn@users.noreply.github.com>
2025-08-10 13:11:18 -0700

    2 exercises

---
+++ Loops/Countdown.py
@@ -0,0 +1,7 @@
+# change step range to -1 reverse
+# change range start to 10 and end at 0
+
+for i in range(10, 0, -1):
+    print(i)
+
+print("Launch!")
---
+++ Loops/loop_and_print.py
@@ -0,0 +1,5 @@
+# loop below will print value in range by step of 2
+# I expect the numbers to be 0, 2, 4, 6, 8, 10
+
+for num in range(0, 11, 2):
+    print(num)

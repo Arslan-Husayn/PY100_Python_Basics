9360eeb66affac2e3653f27af998d4ac41aaff71
Talha Husayn <220093113+Arslan-Husayn@users.noreply.github.com>
2025-08-11 19:21:52 -0700

    more exercises

--- Loops/Continue.py
+++ Loops/Continue.py
@@ -3,7 +3,17 @@ cities = ['Istanbul', 'Los Angeles', 'Tokyo', None,
 
 for city in cities:
     if city != None:
-        print(city)
+        print(len(city))
     else:
         continue
 
+#not using the else here 
+
+terry = ['terry', 'Los Angeles', 'Tokyo', None,
+          'Vienna', None, 'London', 'Beijing', None]
+
+for city in terry:
+    if city is None:
+        continue
+
+    print(len(city))

1a1c4a5657351540e740b44559df4b108faf706c
Talha Husayn <220093113+Arslan-Husayn@users.noreply.github.com>
2025-08-11 19:23:18 -0700

    push to github

---
+++ Loops/And_on_and_on.py
@@ -0,0 +1,21 @@
+
+
+
+while True:
+    print("and on")
+    break
+
+#testing a while loop for a number guessing game
+
+import random
+
+secret_number = random.randint(1,7)
+
+while True:
+    guess = int(input("Guess between 1 and 7: "))
+
+    if guess == secret_number:
+        print("You got that shit! 🎉")
+        break
+    else:
+        "Nope, do it again sucka"

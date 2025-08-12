4d4f87da7564e7a1028826e5a7c33d662463c503
Talha Husayn <220093113+Arslan-Husayn@users.noreply.github.com>
2025-08-11 19:00:06 -0700

    update to exercise

--- Loops/greet_your_friends.py
+++ Loops/greet_your_friends.py
@@ -3,6 +3,12 @@ friends = ['Sarah', 'John', 'Hannah', 'Dave']
 for i in range(0, len(friends)):
     print("Hello, " + friends[i] + '!')
 
+print("             ")
+
 for friend in friends:
     print("Hello, " + friend + '!')
 
+print("             ")
+
+for friend in friends:
+    print(f"Hello, {friend}!")

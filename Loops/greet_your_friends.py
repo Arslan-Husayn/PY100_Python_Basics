9c0567f72b21cdd256660ba03678d8757f0ed2c1
Talha Husayn <220093113+Arslan-Husayn@users.noreply.github.com>
2025-08-11 18:25:17 -0700

    more exercises

---
+++ Loops/greet_your_friends.py
@@ -0,0 +1,8 @@
+friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+for i in range(0, len(friends)):
+    print("Hello, " + friends[i] + '!')
+
+for friend in friends:
+    print("Hello, " + friend + '!')
+
---
+++ Loops/myownlistexer.py
@@ -0,0 +1,7 @@
+words = ["cat", "banana", "elephant", "sun", "strawberry", "dog"]
+index = 0
+
+while index < len(words):
+    if len(words[index]) > 5:
+        print(words[index])
+    index += 1

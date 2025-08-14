7e6d94508eb6c35e1b77edfd7dfc46e87d3a26d2
Talha Husayn <220093113+Arslan-Husayn@users.noreply.github.com>
2025-08-13 20:41:28 -0700

    pushing this up to github

---
+++ Conditionals/check_weather_part2.py
@@ -0,0 +1,9 @@
+weather = 'sunny'
+
+match weather:
+    case 'sunny':
+        print("It's a beautiful day!")
+    case 'rainy':
+        print("Grab your umbrella")
+    case 'horse':
+        print("holy shit wtf")
